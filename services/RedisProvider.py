a2c9ec2a48ad39aa8b8d5cb39d09677b05f5bedf
Ryan Gordon <ryangordon210@gmail.com>
2018-04-23 18:00:57 +0100

    Got routes working
    Data is a little weird but okay for now
    Next is adding to the leaderboard

--- services/RedisProvider.py
+++ services/RedisProvider.py
@@ -8,11 +8,11 @@ import json
 class RedisProvider(object):
     def __init__(self, items: list=[]):
         self._items = items
-        
+        self.formatted_results = []
         
         
     def getLeaderboard(self) -> str:
-
+        self.formatted_results = []
         #Connect to redis
         pool = redis.ConnectionPool(host='192.168.1.8', port=6379, db=0)
         r = redis.Redis(connection_pool=pool)
@@ -20,15 +20,18 @@ class RedisProvider(object):
         # Run zrevrange from docs zrevrange(name, start, end, withscores=False, score_cast_func=<type 'float'>)
         raw_results = r.zrevrange(str('score.quizscore:'+datetime.datetime.today().strftime('%Y-%m-%d')) , 0, 5, withscores=True) 
         print(raw_results)
+
         for score in raw_results:
             print(score)
             print("\n\n Score before:"+score[0].decode("utf-8"))
-            print("\n\n User : "+score[0].decode("utf-8")+ " with Score : "+str(score[1]))
+            print("\n\n User : "+score[0].decode("utf-8")+ " with Score : "+str(score[1]) )
+
+            self.formatted_results.append([score[0].decode("utf-8"), score[1]])
        #Format results 
 
        #Return
 
-        return json.dumps(raw_results), 200
+        return json.dumps(self.formatted_results), 200
 
     def getTodaysCurrentChampion(self) -> str:
 
@@ -44,4 +47,4 @@ class RedisProvider(object):
         score = results[0]
         #Return
 
-        return "Todays champion is : \n User : "+str(score[0])+ " with Score : "+str(score[1]), 200
+        return "Todays champion is : \n User : "+str(score[0].decode("utf-8"))+ " with Score : "+str(score[1]), 200
